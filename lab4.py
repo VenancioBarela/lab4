33aadaad184c56d640c271e5d7146b42494bc5e1
Venancio Barela <calivee.ca@gmail.com>
2022-05-16 16:15:56 -0700

    finished pseudocode

--- lab4.py
+++ lab4.py
@@ -10,3 +10,23 @@ If the user could not answer the question with the max attempts, then print "You
 then print "The correct answer is "Sacramento".
 
 """
+
+"""
+main
+  question = "What is the capital of California"
+  Answer = "Sacramento"
+  ask(question,answer)
+
+ask 
+    tries = 0
+    loop three times
+        increment tries by 1
+        ask user input()
+        check to see if user input is equal to answer
+            if so, print "Correct" then exit the loop
+    if not correct
+        print to the user "You have used up your allotment of guesses." 
+        print the correct answer "The correct answer is 'Sacramento'"
+ 
+main
+"""
